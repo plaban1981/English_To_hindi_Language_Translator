23ca2ccc7acce6d01a665afa40b09fe84ffc135e
PLABAN NAYAK <plaban1981@users.noreply.github.com>
2022-11-12 00:46:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ with st.form("Prediction_form"):
    submit = st.form_submit_button("Translate Text to Hindi")
    #
    if submit:
-        tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M",use_auth_token=access_token)
-        model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M",use_auth_token=access_token)
+        tokenizer = AutoTokenizer.from_pretrained(r"model_files/tk",use_auth_token=access_token)
+        model = AutoModelForSeq2SeqLM.from_pretrained(r"model_files/md",use_auth_token=access_token)
         inputs = tokenizer(text, return_tensors="pt")
 
         translated_tokens = model.generate(**inputs, 
